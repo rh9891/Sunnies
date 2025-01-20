b3f725ebe49b582fe3fc3b6f0785a84f1dd52a47
rh9891 <romie.hecdivert@gmail.com>
2025-01-20 00:18:46 -0500

    Updated image to remove white space. Modified Hero and Footer components.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ type FooterProps = {
 
 export default function Footer({className}: FooterProps) {
     return (
-        <footer>
+        <footer className={className}>
             <img
                 src="Sydney.png"
                 alt="Sydney, Australia cityscape"/>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,7 +13,7 @@ export default function Hero({}: HeroProps) {
         <div className='py-4 sm:py-10 flex flex-col gap-4 sm:gap-8'>
             <h1 className={'text-5xl sm:text-text-6xl md:text-7xl text-center ' + atmaSans.className}>Reflect, grow, and
                 embrace <span className='text-gradient'>brighter</span> days with <span
-                    className='textGradient'>Sunnies</span>.</h1>
+                    className='text-gradient'>Sunnies</span>.</h1>
             <p className='text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[600px]'>Track your journey,
                 share insights, and <span className='font-semibold'>stay positive every day</span>.
             </p>
