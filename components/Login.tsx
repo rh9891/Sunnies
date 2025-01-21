afa6812af6b171541a4a9122ef7e3a36e8ce0f05
rh9891 <romie.hecdivert@gmail.com>
2025-01-21 01:58:05 -0500

    Completed login page ui.

--- components/Login.tsx
+++ components/Login.tsx
@@ -11,7 +11,7 @@ type LoginProps = {
 export default function Login({className}: LoginProps) {
     return (
         <div className={'flex flex-col flex-1 justify-center items-center gap-4 ' + className}>
-            <h3 className={'text-4xl sm:text-5xl md:text-6xl ' + atmaSans.className}> Log In or Register</h3>
+            <h3 className={'text-4xl sm:text-5xl md:text-6xl ' + atmaSans.className}>Log In to Your Account</h3>
             <p className={nunitoSans.className}>You&#39;re one step away from a brighter life.</p>
             <input
                 className="w-full max-w-[400px] mx-auto px-3 duration-200 hover:border-yellow-500 focus:border-yellow-500 py-2 sm:py-3 border border-solid border-yellow-400 rounded-full outline-none"
@@ -19,8 +19,10 @@ export default function Login({className}: LoginProps) {
             <input
                 className="w-full max-w-[400px] mx-auto px-3 duration-200 hover:border-yellow-500 focus:border-yellow-500 py-2 sm:py-3 border border-solid border-yellow-400 rounded-full outline-none"
                 placeholder='Password' type="password"/>
-            <div className='max-w-[400px] w-full mx-auto'>
+            <div className='max-w-[400px] w-full mx-auto flex flex-col gap-4'>
                 <Button text="Submit" variant="light" full/>
+                <p className={nunitoSans.className + ' text-center'}>Don&#39;t have an account? <span
+                    className='text-gradient'>Sign Up</span></p>
             </div>
         </div>
     )
