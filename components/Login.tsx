76c4a1c35a2337d2dfc8712800dbeb2916e12402
rh9891 <romie.hecdivert@gmail.com>
2025-02-09 09:38:52 -0500

    Added functionality to toggle between register and login page.

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from "react";
+"use client";
+import { ReactNode, useState } from "react";
 
 import { atmaSans, nunitoSans } from "@/fonts";
 import Button from "@/components/Button";
@@ -9,6 +10,14 @@ type LoginProps = {
 };
 
 export default function Login({ className }: LoginProps) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isRegistering, setIsRegistering] = useState(false);
+
+  const handleSubmit = async () => {
+    console.log(email, password);
+  };
+
   return (
     <div
       className={
@@ -16,16 +25,20 @@ export default function Login({ className }: LoginProps) {
       }
     >
       <h3 className={"text-4xl sm:text-5xl md:text-6xl " + atmaSans.className}>
-        Log In to Your Account
+        {isRegistering ? "Start Your Journey" : "Log In to Your Account"}
       </h3>
       <p className={nunitoSans.className}>
         You&#39;re one step away from a brighter life.
       </p>
       <input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
         className="w-full max-w-[400px] mx-auto px-3 duration-200 hover:border-yellow-500 focus:border-yellow-500 py-2 sm:py-3 border border-solid border-yellow-400 rounded-full outline-none"
         placeholder="Email"
       />
       <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
         className="w-full max-w-[400px] mx-auto px-3 duration-200 hover:border-yellow-500 focus:border-yellow-500 py-2 sm:py-3 border border-solid border-yellow-400 rounded-full outline-none"
         placeholder="Password"
         type="password"
@@ -33,8 +46,15 @@ export default function Login({ className }: LoginProps) {
       <div className="max-w-[400px] w-full mx-auto flex flex-col gap-4">
         <Button text="Submit" variant="light" full />
         <p className={nunitoSans.className + " text-center"}>
-          Don&#39;t have an account?{" "}
-          <span className="text-gradient">Sign Up</span>
+          {isRegistering
+            ? "Already have an account? "
+            : "Don\'t have an account? "}
+          <button
+            onClick={() => setIsRegistering(!isRegistering)}
+            className="text-gradient"
+          >
+            {isRegistering ? "Log In" : "Sign Up"}
+          </button>
         </p>
       </div>
     </div>
